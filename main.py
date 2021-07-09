68a9a20937b62325ce95fdc61d684c399b8cf8a9
Bharat Mukheja <bharat.mukheja@gmail.com>
2021-07-08 22:33:30 -0700

    Added project ideas

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import platform
 from pynotifier import Notification
 
+# Notification destinations - Desktop, Slack, Discord, Email, Android/iOS, DBus, Multicast, IP unicast
+# Notification sources - Files, Process, Filtered file/process
+
 def system_notification(title:str,message:str='Message Body Empty'):
     Notification(title=title,description=message).send()
 
